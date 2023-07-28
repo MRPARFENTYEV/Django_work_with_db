ebae999855b55230175e99e67524c5076a234f8e
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-07-28 03:55:03 +0300

    bilding commit 1

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, unique=True)
+    name = models.CharField(max_length=50, null=False)
+    price = models.IntegerField(max_length=10)
+    image = models.ImageField(max_length=100)
+    release_date = models.DateField(max_length=10)
+    lte_exists = models.CharField(max_length=10)
+    slug = models.SlugField(max_length=200,db_column=name,unique=True)
+
     # TODO: Добавьте требуемые поля
     pass
